80e998e4e72721111d05a078dfc2c09450a6522d
cristianrcaja <cristianrcaja@hotmail.com>
2024-04-24 22:09:01 -0300

    Estudando...

--- src/Diaria.java
+++ src/Diaria.java
@@ -3,11 +3,16 @@ public class Diaria extends Despesa {
 
     @Override
     public void calcularDespesa() {
+    super.cadastrarDespesa();
+        System.out.println("Qual a quantidade de diarias utilizadas?: ");
+    this.qtdeDiaria = leitor.nextDouble();
+    super.setValorTotal(qtdeDiaria * 50 );
 
     }
 
     @Override
     public void listarDespesa() {
+        System.out.println(qtdeDiaria + getDescricao() + getValorTotal());
 
     }
 
